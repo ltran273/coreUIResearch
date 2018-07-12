4cfa84ff61b43b08900ecaaf1ee34a16540d1f4b
Linh <hlinh273@gmail.com>
2018-07-12 09:29:52 +0700

    Initial

--- src/routes.js
+++ src/routes.js
@@ -182,11 +182,19 @@ const User = Loadable({
   loading: Loading,
 });
 
+const HomeView = Loadable({
+  loader: () => import('./views/Home/Home'),
+  loading: Loading,
+})
+
 
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
+
+  { path: '/home', name: 'Home1', component: HomeView },
+
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/theme', exact: true, name: 'Theme', component: Colors },
   { path: '/theme/colors', name: 'Colors', component: Colors },
